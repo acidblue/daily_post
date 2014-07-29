f8a2c5960a273ff876f8618672484de58077ac53
acidblue <sunblaster5@gmail.com>
2014-07-29 15:21:35 -0700

    Finished layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,13 +2,26 @@ require 'spec_helper'
 
 
   describe "Static pages" do
-  	describe "Home page" 
-    it "should have the content 'Daily Post'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Daily Post')
+	
+  	subject {page}
+  	describe "Home Page" do
+	      before { visit root_path }
+	      
+    it { should have the content('Daily Post')}
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
     end
-    it "should have the base titile" do
-    	visit '/static_pages/home'
-    	expect(page).not_to have_title('| Home')
-    end
-end
+
+describe "Help Page" do
+      before { visit help_path }
+      
+      it { shold have content('Help') }
+      it { should have_title(full_title('Help')) }
+      end
+
+describe "About page" do
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
